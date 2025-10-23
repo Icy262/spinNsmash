05701a0f069b22b6ada99fe6105baa0868593023
Icy26 <114495528+Icy262@users.noreply.github.com>
2025-10-22 23:39:41 -0400

    Add Player. Contains a socket connection, and a flag indicating if it is a real player or a bot.

---
+++ server/player.py
@@ -0,0 +1,4 @@
+class Player:
+	def __init__(self, connection, real_player):
+		self.client_connection = connection #the player's connection if it is a real person, or None if a bot
+		self.real_player = real_player #True if a real player, false if a bot
