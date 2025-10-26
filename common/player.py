074600ae6bbb675e17d56a836128744829293544
Icy26 <114495528+Icy262@users.noreply.github.com>
2025-10-26 11:38:29 -0400

    Mover Player to common and add position fields

--- common/player.py
+++ common/player.py
@@ -1,4 +1,6 @@
 class Player:
-	def __init__(self, connection, real_player):
+	def __init__(self, connection, real_player, x = 0, y = 0):
+		self.x = x
+		self.y = y
 		self.client_connection = connection #the player's connection if it is a real person, or None if a bot
 		self.real_player = real_player #True if a real player, false if a bot
