5669b4c9ee233dec58200bf5fec8b0da42d99d20
Icy26 <114495528+Icy262@users.noreply.github.com>
2025-11-24 13:11:35 -0500

    add a game to the games list at startup, and work with the client_connection -> network_connection name change

--- server/main.py
+++ server/main.py
@@ -1,8 +1,9 @@
 import socket
 import threading
-from common.client_connection import ClientConnection
+from common.network_connection import NetworkConnection
 from server.lobby import GameLobby
 from server.player import Player
+from server.game import Game
 
 #the config should be moved into a json at some point
 #adding debugging to log files would be a good idea
@@ -10,18 +11,20 @@ from server.player import Player
 max_games = 3 #maximum of 3 games should be running on this server at any given time
 port = 4004 #This port is pretty easy to remember and doesn't seem to be in widespread use
 
-client_connection_listener = socket.socket() #create a socket for the server to use to accept client connections
-client_connection_listener.bind(('', port)) #bind the socket. the bind function expects a pair containing an ip and port. using '' as the ip causes the socket to bind to all interfaces.
-client_connection_listener.listen() #set the socket to listen mode
+network_connection_listener = socket.socket() #create a socket for the server to use to accept client connections
+network_connection_listener.bind(('', port)) #bind the socket. the bind function expects a pair containing an ip and port. using '' as the ip causes the socket to bind to all interfaces.
+network_connection_listener.listen() #set the socket to listen mode
 
 game_lobby = GameLobby() #create a game lobby to pass players into once they connect
 games = [] #a list of running games
+games.append(Game(3000, 3000, 0)) #new game
 
 threading.Thread(target = game_lobby.player_handler, args = (games,)).start() #start the game_lobby's player handling
+threading.Thread(target = games[0].run).start() #start the main game loop
 
 while True:
-	socket_connection = client_connection_listener.accept()[0] #blocks the thread until a new connection arrives. when a connection arrives, accepts, and stores the connection. accept() returns a tuple with the connection and the address, we use [0] to just take the connection returned
-	new_client_connection = ClientConnection(socket_connection) #makes a new client connection object with the socket connection
-	player = Player(new_client_connection, True) #create a new player for this player connection, and designate it as a real player
+	socket_connection = network_connection_listener.accept()[0] #blocks the thread until a new connection arrives. when a connection arrives, accepts, and stores the connection. accept() returns a tuple with the connection and the address, we use [0] to just take the connection returned
+	new_network_connection = NetworkConnection(socket_connection) #makes a new client connection object with the socket connection
+	player = Player(new_network_connection, True) #create a new player for this player connection, and designate it as a real player
 	game_lobby.add_player(player) #transfers the player to the game_lobby
-	threading.Thread(target = new_client_connection.recieve).start() #start a new thread to listen for messages from the client
+	threading.Thread(target = new_network_connection.recieve).start() #start a new thread to listen for messages from the client
