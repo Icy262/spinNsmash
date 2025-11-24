d21f4490a69360e847b02f5b7291bb7fab3bc4e1
Icy26 <114495528+Icy262@users.noreply.github.com>
2025-11-24 13:26:51 -0500

    resolve a bug relating to class vs instance variables

--- server/player_container.py
+++ server/player_container.py
@@ -6,7 +6,8 @@ class PlayerContainer(abc.ABC):
 	"""
 	This should be extended to provide thread-safe functionality for any class that works with players (eg. the game lobby, or a game instance).
 	"""
-	connected_players = [] #holds all the players being contained in this container
+	def __init__(self):
+		self.connected_players = [] #holds all the players being contained in this container
 
 	def add_player(self, player):
 		"""
@@ -20,7 +21,8 @@ class PlayerContainer(abc.ABC):
 		Transfers a player from this instance to another instance of PlayerContainer. If the player isn't in the player list here, does nothing
 		"""
 		with threading.Lock(): #thread safety is fun
-			if(self.connected_players.remove(player) != None): #removes the player from our list. list.remove() returns the value it removed. If this is None, it means the player is not in the list. We need to handle this because if not you will get None values getting added to other containers
+			if(player in self.connected_players): #removes the player from our list. list.remove() returns the value it removed. If this is None, it means the player is not in the list. We need to handle this because if not you will get None values getting added to other containers
+				self.connected_players.remove(player)
 				other_container.add_player(player) #adds the player to the other container
 			else: #player wasn't in player list
-				print("PLAYER NOT CONNECTED") #SHOULD PROBABLY HANDLE THIS PROPERLY LATER
+				print("PLAYER NOT CONNECTED") #TODO: SHOULD PROBABLY HANDLE THIS PROPERLY LATER
