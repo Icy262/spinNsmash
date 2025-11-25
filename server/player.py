2875013f5691c5b8b1cbcb548b6f1447d0f95f38
Icy26 <114495528+Icy262@users.noreply.github.com>
2025-11-25 12:44:42 -0500

    add player_entity field, and flag for if player_entity has been updated in a way that requires the player to change the local copy of player

--- server/player.py
+++ server/player.py
@@ -1,5 +1,7 @@
 class Player:
-	def __init__(self, network_connection, real):
-		#None for connection if false
+	def __init__(self, network_connection, real, player_entity = None):
+		#None for connection if real is False
 		self.network_connection = network_connection
 		self.real = real
+		self.player_entity = player_entity
+		self.player_entity_updated = True
